dff460930bf899422cba208a37082aedd647ab0e
timop <tpalm3186@gmail.com>
2024-05-27 20:16:39 +0200

    Implement interactive anagram service

---
+++ src/main/kotlin/tp/anagraminteractive/AnagramInteractiveApplication.kt
@@ -0,0 +1,44 @@
+package tp.anagraminteractive
+
+import org.example.service.AnagramService
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class AnagramInteractiveApplication
+
+val anagramService = AnagramService()
+
+fun main(args: Array<String>) {
+    runApplication<AnagramInteractiveApplication>(*args)
+
+    println(
+        "Hello. This Program provides 2 functions. Function 1 checks if two words are anagrams" +
+                " and stores if that the case. Function 2 returns previously searched anagrams of a word. "
+    )
+    while (true) {
+        checkFunction()
+    }
+}
+
+private fun checkFunction() {
+
+    println("Do you want to call the first or the second function? Please Enter 1 or 2:")
+    val func = readLine()
+
+    if (func == "1") {
+        println("Enter the first word to be checked.")
+        val word1: String = readln()
+        println("Enter the second word to be checked.")
+        val word2: String = readln()
+
+
+        println(anagramService.anagramCheckerStoreAtMatch(word1, word2))
+    } else if (func == "2") {
+
+        println("Enter the word, you would like to find the previously stored anagrams for.")
+        val word: String = readln()
+        println(anagramService.findForPreviouslySearchedAnagramsOf(word))
+    }
+
+}
---
+++ src/main/kotlin/tp/anagraminteractive/service/AnagramService.kt
@@ -0,0 +1,109 @@
+package org.example.service
+
+import org.springframework.stereotype.Service
+import java.util.*
+
+
+private const val ASCII_CHAR_NUMBER = 256
+
+/**
+ * Service to handle anagrams.
+ *
+ * English wiki definition: An anagram is a word or phrase formed
+ * by rearranging the letters of a different word or phrase,
+ * typically using all the original letters exactly once.
+ *
+ * Remark: Whitespaces can be added or removed
+ * Example: "New York Times" = "monkeys write"
+ *
+ */
+@Service
+class AnagramService {
+    private val sortedAnagramToAssociatedWords: MutableMap<String, LinkedHashSet<String>> = mutableMapOf()
+
+    /**
+     *    Straightforward implementation, O(NlogN) --> Fine for Small N, so fine for realistic application
+     *    such as sentences or phrases.
+     */
+    fun anagramCheckerStoreAtMatch(word1: String, word2: String): Boolean {
+
+        val orderedWord1 = prepareAndSort(word1)
+        val orderedWord2 = prepareAndSort(word2)
+
+        if (orderedWord1.length != orderedWord2.length) return false
+        if (orderedWord1 == orderedWord2) {
+            fillMemoryMap(orderedWord1, word1, word2)
+            return true
+        }
+        return false
+    }
+
+    /**
+     * Return associated set of anagrams. Only return Anagrams if input word has been previously used in check function.
+     */
+    fun findForPreviouslySearchedAnagramsOf(word: String): MutableSet<String> {
+        return getAssociatedSet(
+            sortedAnagramToAssociatedWords[prepareAndSort(word)],
+            word
+        )
+    }
+
+    private fun getAssociatedSet(
+        associatedSet: LinkedHashSet<String>?,
+        word: String
+    ): MutableSet<String> {
+        if (associatedSet != null) {
+            if (associatedSet.contains(word)) associatedSet.remove(word) else return mutableSetOf()
+            return associatedSet
+        } else return mutableSetOf()
+    }
+
+
+    private fun prepareAndSort(word: String): String {
+        val charArray = prepareForAnagramCheck(word)
+        Arrays.sort(charArray)
+        return String(charArray)
+    }
+
+    private fun prepareForAnagramCheck(word: String) = word.filter { !it.isWhitespace() }.lowercase().toCharArray()
+
+    private fun fillMemoryMap(orderedWord: String, word1: String, word2: String) =
+        sortedAnagramToAssociatedWords.computeIfAbsent(
+            orderedWord
+        ) { linkedSetOf() }
+            .addAll(setOf(word1, word2))
+
+
+    /**
+     * Alternative, disregarded Method
+     * Faster Method for big Input, but less straight-forward and requiring more memory, 256 for ASCII rep
+     * O(N), 2 For-Loops, one fixed at ASCII Code size (assuming ascii chars, can be reduced by ordering indices list)
+     * -> slower for small words.
+     *
+     * Also, a book is technically not a phrase != english wiki definition.
+     */
+    private fun checkAnagramFastForLarge(word1: String, word2: String): Boolean {
+
+        val count = IntArray(ASCII_CHAR_NUMBER)
+        val countForKey = IntArray(ASCII_CHAR_NUMBER)
+        val string1NoWhiteSpaces = prepareForAnagramCheck(word1)
+        val string2NoWhiteSpaces = prepareForAnagramCheck(word2)
+
+        val reducedLength = string1NoWhiteSpaces.size
+        if (reducedLength != string2NoWhiteSpaces.size) return false
+
+        for (i in 0..< reducedLength) {
+            count[string1NoWhiteSpaces[i].code]++
+            count[string2NoWhiteSpaces[i].code]--
+
+            countForKey[string1NoWhiteSpaces[i].code]++
+        }
+        var orderedAnagram = ""
+        for (i in 0..< ASCII_CHAR_NUMBER) {
+            if (count[i] != 0) return false
+            for (j in 0..<countForKey[i]) orderedAnagram += Char(i)
+        }
+        fillMemoryMap(orderedAnagram, word1, word2)
+        return true
+    }
+}
